b0d076b3d84c90e2d7aed2b38a35adfb5b2c1428
Akashsingh Sisodiya <akashsinghsisodiya@gmail.com>
2020-05-20 23:58:45 +0530

    Feature: Calculator Update
    Feature: Calculator Update

--- 11Calculator/script.js
+++ 11Calculator/script.js
@@ -3,12 +3,25 @@ function calculate(params) {
         calc.txt.value = 0;
     }
     try {
-        document.calc.txt.value = eval(calc.txt.value);
+        let ans = eval(calc.txt.value);
+        // console.log(ans)
+        if (isNaN(ans) || !isFinite(ans)) {
+            // console.log(ans);
+            throw "Error"
+        } else {
+            document.calc.txt.value = ans;
+        }
     } catch (error) {
         document.getElementById("warning").style.display = "grid";
         document.getElementById("warning").style.animation = "fadeout ease-in 3s";
         setTimeout(function () {
             document.getElementById("warning").style.display = "none";
         }, 3000);
+    }
+}
+
+function deleteLast() {
+    if(document.calc.txt.value.length > 0) {
+        document.calc.txt.value = document.calc.txt.value.substring(0, document.calc.txt.value.length - 1)
     }
 }
