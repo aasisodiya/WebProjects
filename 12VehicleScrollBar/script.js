4b1826348696030ce2375d57b2e08ca731e697e2
Akashsingh Sisodiya <akashsinghsisodiya@gmail.com>
2020-05-22 02:06:15 +0530

    Feature: Vehicle Horizontal Scroll (#6)
    * Feature: Calculator Update
    
    Feature: Calculator Update
    
    * Feature: Vehicle Horizontal Scroll
    
    Feature: Vehicle Horizontal Scroll

---
+++ 12VehicleScrollBar/script.js
@@ -0,0 +1,32 @@
+let debug = document.getElementById("debug");
+let debugging = true;
+
+let directionHelper = 0;
+let truckicon = document.getElementById("itruck");
+let truck = document.getElementById("truck");
+let trucksize = truckicon.offsetWidth
+
+document.onscroll = function (params) {
+    let netScrollableHeight = document.body.scrollHeight - window.innerHeight;
+    let scrollProgress = ((window.pageYOffset) / (netScrollableHeight)) * 100;
+
+    if (debugging) {
+        let debugdata = "Net Scrollable Height:" + netScrollableHeight + " Scroll Progress:" + scrollProgress;
+        debug.innerHTML = debugdata;
+    }
+
+    // Relatively calculating Horizontal displacement of truck based on vertical scroll
+    let roadLength = window.innerWidth - trucksize; //trucksize is used to stop truck before crashing :-P
+    let displacement = roadLength * (scrollProgress / 100);
+
+    // Setting Truck Direction
+    if ((displacement - directionHelper) > 0) {
+        truckicon.style.transform = "rotateY(180deg)";
+    } else {
+        truckicon.style.transform = "rotateY(0deg)";
+    }
+
+    // Setting Displacement
+    truck.style.left = displacement + "px";
+    directionHelper = displacement;
+}
