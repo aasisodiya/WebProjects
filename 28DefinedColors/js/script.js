db9feefbdfe14e482f0da922da4dde22a8681fcf
Akashsingh Sisodiya <akashsinghsisodiya@gmail.com>
2021-04-11 00:56:05 +0530

    Update for Use Strict (#52)
    * Feature: Calculator Update
    
    Feature: Calculator Update
    
    * Feature: Vehicle Horizontal Scroll
    
    Feature: Vehicle Horizontal Scroll
    
    * Feature: Isometric and Neumorphic Webpage
    
    Feature: Isometric and Neumorphic Webpage
    
    * Feature: Device Rotation
    
    Feature: Device Rotation
    
    * Feature: Menu
    
    Feature: Menu
    
    * Update: Index
    
    Update: Index
    
    * Feature: Masonry Grid
    
    Feature: Masonry Grid
    
    * Documentation: on(), off(), one() and click()
    
    Comparison of .on() method vs .click() method
    on() Method
    Using eventsMap in on()
    Using on() with event, data and handler
    Using on() with event, selector and handler
    off() Method
    one() Method
    
    * Documentation: Date in JavaScript
    
    Documentation: Date in JavaScript
    
    * Updated Index
    
    * Documentation: target Vs currentTarget
    
    Documentation: target Vs currentTarget property
    
    * Documentation: Vertically Divided Element Layout
    
    Documentation: Vertically Divided Element Layout
    
    * Documentation: Solution to undefined 404 error
    
    Documentation: Solution to undefined 404 error
     **Error**: `(index):1 GET https://localhost/myproject/undefined 404`
    
    * Updated Index
    
    Updated Index
    
    * Updated Index
    
    Updated Index
    
    * Updated Documentation
    
    * Create Copy Of An Object In JavaScript
    
    Create Copy Of An Object In JavaScript
    
    * Documentation: Detect if user has seen/visited an Element
    
    Documentation: Detect if user has seen/visited an Element of website
    
    * Feature: Animated Logo
    
    Feature: Animated Logo
    
    * Updated Index
    
    Updated Index
    
    * Feature: My First Public Website
    
    Feature: My First Public Website
    
    * Feature: Scroll Activated CSS
    
    Feature: Old Project
    
    * Feature: Simplistic Transition Animation
    
    Feature: Simplistic Transition Animation
    
    * Documentation: Defined Colors
    
    Documentation: Defined Colors
    
    * Index Updated
    
    Index Updated
    
    * Notes: Concepts Used In GradientPlay
    
    Notes: Concepts Used In GradientPlay
    
    * Feature: Creative Backgrounds
    
    Feature: Creative Backgrounds
    
    * Feature: Parallax Background
    
    Feature: Parallax Background
    
    * Update: Cursor Over Navigation Arrow Converted To Pointer
    
    Update: Cursor Over Navigation Arrow Converted To Pointer
    
    * Documentation: Vertically And Horizontally Center
    
    Documentation: Vertically And Horizontally Center
    
    * Documentation: Backdrop-Filter
    
    Documentation: Backdrop-Filter
    
    * Update .gitignore
    
    * Index Updated
    
    * Documentation: Calc() in CSS
    
    Documentation: Calc() in CSS
    
    * Code Updated

--- 28DefinedColors/js/script.js
+++ 28DefinedColors/js/script.js
@@ -1,5 +1,5 @@
 // Template Code Start
-`use strict`
+'use strict'
 // Just for Fun!
 console.log('%c Stop Right There! ', 'background: #222; color: orange;font-size:20px');
 console.log('%c You Shall Not Pass! ', 'background: #222; color: red; font-size:40px');
