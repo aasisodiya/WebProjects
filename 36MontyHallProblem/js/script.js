b4ef494524f1e32f62aac8999449450cf84d13d5
Akashsingh Sisodiya <akashsinghsisodiya@gmail.com>
2021-05-30 00:23:04 +0530

    Feature: Monty Hall Problem (#105)
    Feature: Monty Hall Problem

--- 36MontyHallProblem/js/script.js
+++ 36MontyHallProblem/js/script.js
@@ -1,8 +1,14 @@
 // Template Code Start
-"use strict"
+"use strict";
 // Just for Fun!
-console.log('%c Stop Right There! ', 'background: #222; color: orange;font-size:20px');
-console.log('%c You Shall Not Pass! ', 'background: #222; color: red; font-size:40px');
+console.log(
+    "%c Stop Right There! ",
+    "background: #222; color: orange;font-size:20px"
+);
+console.log(
+    "%c You Shall Not Pass! ",
+    "background: #222; color: red; font-size:40px"
+);
 // Template Code End
 
 // // shuffle function to shuffle the input array
@@ -28,60 +34,64 @@ let carIndex;
 let doorClickCounts = 0;
 
 function startGame() {
-    $('#action2').hide();
-    $('#action3').hide();
-    $('#action4').hide();
-    $('#action1').show();
-    $('.donkey').removeClass('donkey');
-    $('.car').removeClass('car');
-    $('.dooropen').removeClass('dooropen');
-    $('.object').addClass('donkey');
-    carIndex = Math.floor((Math.random() * 3));
-    $(`.holder:eq(${carIndex}) > .object`).addClass('car').removeClass('donkey');
+    $('.selected').hide();
+    $("#action2").hide();
+    $("#action3").hide();
+    $("#action4").hide();
+    $("#action1").show();
+    $(".donkey").removeClass("donkey");
+    $(".car").removeClass("car");
+    $(".dooropen").removeClass("dooropen");
+    $(".object").addClass("donkey");
+    carIndex = Math.floor(Math.random() * 3);
+    $(`.holder:eq(${carIndex}) > .object`).addClass("car").removeClass("donkey");
     doorClickCounts = 0;
 }
 
-$('#help').on('click', function () {
-    $('#message').toggle();
+$("#help").on("click", function () {
+    $("#message").toggle();
 });
 
-$('#message').hide();
+$("#message").hide();
 
-$('.door').on('click', function (event) {
+$(".door").on("click", function (event) {
+    // Add the selected icon to user selected door
+    $('.selected').hide();
+    let doorNo = $(".door").index(this);
+    $(`.selected:eq(${doorNo})`).show();
     // first click
     if (doorClickCounts == 0) {
-        $('#action1').hide();
-        $('#action2').show();
+        $("#action1").hide();
+        $("#action2").show();
         let donkeyIndex;
         if ($(this).siblings().attr("class").includes("donkey")) {
-            donkeyIndex = $('.donkey').index($(this).siblings());
+            donkeyIndex = $(".donkey").index($(this).siblings());
             if (donkeyIndex == 0) {
                 donkeyIndex = 1;
             } else {
                 donkeyIndex = 0;
             }
         } else {
-            donkeyIndex = Math.floor((Math.random() * 2));
+            donkeyIndex = Math.floor(Math.random() * 2);
         }
-        $(`.donkey:eq(${donkeyIndex})`).parent().find('.door').addClass('dooropen');
+        $(`.donkey:eq(${donkeyIndex})`).parent().find(".door").addClass("dooropen");
         doorClickCounts++;
     } else {
         // open all doors
-        $('#action1').hide();
-        $('#action2').hide();
-        $('.door').addClass('dooropen');
+        $("#action1").hide();
+        $("#action2").hide();
+        $(".door").addClass("dooropen");
 
         if ($(this).siblings().attr("class").includes("donkey")) {
-            $('#action4').show();
+            $("#action4").show();
         } else {
-            $('#action3').show();
+            $("#action3").show();
         }
 
         setTimeout(function () {
             startGame();
         }, 5000);
     }
-})
-
+});
 
 startGame();
