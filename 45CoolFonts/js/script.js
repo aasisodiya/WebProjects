44a461ace685b092f11c959ba065ef5193f105a8
Akashsingh Sisodiya <akashsinghsisodiya@gmail.com>
2021-07-24 01:43:20 +0530

    Feature: Cool Fonts (#161)
    Copy Code - Logic Implemented
    Feature: Cool Fonts

--- 45CoolFonts/js/script.js
+++ 45CoolFonts/js/script.js
@@ -19,10 +19,24 @@ function copyToClipboard(contentToCopy) {
     input.val(contentToCopy).select();
     document.execCommand("copy");
     input.remove();
+    displayCopiedMessage();
 }
 
-// Font Definition
-class Font {
+// Function to display Copied Message
+function displayCopiedMessage() {
+    let template = `<div class="message">
+        <h3>Copied</h3>
+    </div>`;
+    var copiedmessage = $("<div>");
+    copiedmessage.html(template);
+    $("body").append(copiedmessage);
+    setTimeout(() => {
+        copiedmessage.remove();
+    }, 2000);
+}
+
+// Google Font Definition
+class GoogleFont {
     constructor(fontname) {
         this.fontname = fontname;
         let encoded = fontname.replaceAll(" ", "+");
@@ -32,11 +46,47 @@ class Font {
     }
 }
 
+// Font Definition
+class Font {
+    constructor(fontname, link, importcss, csstag) {
+        this.fontname = fontname;
+        this.link = link;
+        this.importcss = importcss;
+        this.csstag = csstag;
+    }
+}
+
 let fontsCollection = [];
 
 // Push the fonts in array here
-fontsCollection.push(new Font("Raleway"));
-fontsCollection.push(new Font("Raleway Dots"));
+fontsCollection.push(new GoogleFont("Raleway"));
+fontsCollection.push(new GoogleFont("Raleway Dots"));
+fontsCollection.push(new GoogleFont("Exo 2"));
+fontsCollection.push(new GoogleFont("Patua One"));
+fontsCollection.push(new GoogleFont("Fugaz One"));
+fontsCollection.push(new GoogleFont("Racing Sans One"));
+fontsCollection.push(new GoogleFont("Bungee"));
+fontsCollection.push(new GoogleFont("Righteous"));
+fontsCollection.push(new GoogleFont("Roboto Slab"));
+fontsCollection.push(new GoogleFont("Rubik"));
+fontsCollection.push(new GoogleFont("Orbitron"));
+fontsCollection.push(new GoogleFont("Electrolize"));
+fontsCollection.push(new GoogleFont("Bungee Shade"));
+fontsCollection.push(new GoogleFont("Lilita One"));
+fontsCollection.push(new GoogleFont("Press Start 2P"));
+fontsCollection.push(new GoogleFont("Space Mono"));
+fontsCollection.push(new GoogleFont("Lato"));
+fontsCollection.push(new GoogleFont("Audiowide"));
+fontsCollection.push(new GoogleFont("Poiret One"));
+fontsCollection.push(new GoogleFont("Special Elite"));
+fontsCollection.push(
+    new Font(
+        "VCR OSD Mono",
+        '<link href="https://fonts.cdnfonts.com/css/vcr-osd-mono" rel="stylesheet">',
+        "@import url('https://fonts.cdnfonts.com/css/vcr-osd-mono');",
+        "font-family: 'VCR OSD Mono';"
+    )
+);
 
 // Process the fonts
 fontsCollection.forEach((font) => {
@@ -44,7 +94,7 @@ fontsCollection.forEach((font) => {
     let importcss = font.importcss;
     let csstag = font.csstag;
     let template = `
-    <div class="font">
+    <div class="font" style="${csstag}">
         <div class="fontname">${font.fontname}</div>
         <div class="fontexample">ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@$#%^&*()_+-={}:"<>[];',.?/</div>
         <div class="fontcss">
@@ -54,4 +104,13 @@ fontsCollection.forEach((font) => {
         </div>
     </div>`;
     $("#myfonts").append(template);
+});
+
+// Function to show help
+$("#help").on("click", function (param) {
+    $(".help").show();
+});
+// Function to hide help
+$("#closehelp").on("click", function (param) {
+    $(".help").hide();
 });
